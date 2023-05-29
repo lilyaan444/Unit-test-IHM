1d5e320af9400faafa97e712fcd03ea6a580e193
MULLER Lilyan <lilyan.muller9@etu.univ-lorraine.fr>
2023-05-29 12:03:15 +0000

    Update 13 files
    - /src/controleur/.DS_Store
    - /src/controleur/class_infraction_edit.ts
    - /src/controleur/class_infraction_liste.ts
    - /src/controleur/infraction_edit.ts
    - /src/controleur/infraction_liste.ts
    - /src/modele/.DS_Store
    - /src/modele/connexion.ts
    - /src/modele/data_comprend.ts
    - /src/modele/data_conducteur.ts
    - /src/modele/data_delit.ts
    - /src/modele/data_infraction.ts
    - /src/modele/data_vehicule.ts
    - /src/modele/sqlWeb.ts

--- src/controleur/infraction_liste.ts
+++
@@ -1,8 +0,0 @@
-import {vueInfractionListe} from "../controleur/class_infraction_liste.js"
-
-vueInfractionListe.init( { divTitre        :document.querySelector('[id=div_inf_liste_titre]')
-                        ,btnAjouter     :document.querySelector('[id=btn_inf_ajouter]')
-                        ,tableInfraction     :document.querySelector('[id=table_infraction]')
-                    } 
-                    );
-
